913c87c8256466ea3cd1093053ee3a8c47278471
“Christopher <christopherhileweb@gmail.com>
2022-12-10 17:02:12 -0500

    replacement

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ const moodCtlr = require('./api/controllers/mood');
 const medsCtlr = require('./api/controllers/meds');
 const bowelCtlr = require('./api/controllers/bowel');
 const imageCtlr = require('./api/controllers/image');
-const authCtlr = require('./api/controllers/auth');
-
+const authCtlr = require('./API/controllers/auth');
+const forgotpwdCtlr = require('./api/controllers/forgotpassword');
+const nodemailer = require('nodemailer')
 require("dotenv").config();
 
 const port = process.env.PORT || 8080
@@ -41,6 +42,39 @@ app.use('/api/:version/bowel', bowelCtlr)
 app.use('/api/:version/image', imageCtlr)
 app.use('/api/:version/auth', authCtlr)
 
+
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: process.env.REACT_APP_EMAIL,
+    pass: process.env.REACT_APP_WORD,
+    clientId: process.env.REACT_APP_OAUTH_CLIENTID,
+    clientSecret: process.env.REACT_APP_OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.REACT_APP_OAUTH_REFRESH_TOKEN,
+  },
+ });
+  
+ transporter.verify((err, success) => {
+  err
+    ? console.log(err)
+    : console.log(`=== Server is ready to take messages: ${success} ===`);
+ });
+ let mailOptions = {
+  from: "test@gmail.com",
+  to: process.env.REACT_APP_EMAIL,
+  subject: "Nodemailer API",
+  text: "Hi from your nodemailer API",
+ };
+
+ transporter.sendMail(mailOptions, function (err, data) {
+  if (err) {
+    console.log("Error " + err);
+  } else {
+    console.log("Email sent successfully");
+  }
+ });
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
