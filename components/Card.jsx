3e9835667c6276c92ee71328aa37b9d1d8ab7120
“Christopher <christopherhileweb@gmail.com>
2023-12-03 10:52:21 -0500

    change location of card path

--- components/Card.jsx
+++ components/Card.jsx
@@ -4,23 +4,23 @@ export default async function Card(topic) {
   switch (topic.topic) {
     case "food":
       const entry = await foodLatest();
-      const date = entry[0].createdAt.toLocaleString("en-us", {
-        month: "short",
-        day: "numeric",
-        year: "numeric",
-      });
-      const time = entry[0].createdAt.toLocaleString("en-us", {
-        hour: "numeric",
-        minute: "numeric",
-      });
 
       if (entry.length !== 0) {
+        const date = entry[0].createdAt.toLocaleString("en-us", {
+          month: "short",
+          day: "numeric",
+          year: "numeric",
+        });
+        const time = entry[0].createdAt.toLocaleString("en-us", {
+          hour: "numeric",
+          minute: "numeric",
+        });
         return (
           <div className="bg-food h-80 ">
             <p className="pl-4 pt-4">{date}</p>
             <p className="pl-4">{time}</p>
             <h2 className="text-2xl p-4">{entry[0].title}</h2>
-            <p className="text-center">{entry[0].notes}</p>
+            <p className="text-center px-4">{entry[0].notes}</p>
           </div>
         );
       } else {
